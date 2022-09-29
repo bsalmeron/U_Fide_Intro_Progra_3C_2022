3ba21c35ffd87938f73e150633085a8e2db487a0
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2022-09-28 18:12:17 -0600

    Add files via upload

---
+++ Semana 2/main.py
@@ -0,0 +1,41 @@
+#Lenguajes simpre
+edad=20
+salario=200
+peso=20
+nombre='Brayner'
+
+edad=30
+salario =300
+nombre='Juan'
+
+#Lenguajes estructurados
+listaEdades = (20,34,23,43,54,67)
+
+
+#Tipos de datos
+edad = 20
+edad =27
+edad = '29'
+edad ='29 28 33 *$%&&/(&/)(/'
+
+
+#asignacion
+
+
+edad = 20
+
+
+numero1 = 2
+
+
+numero1 += 3
+
+
+print(numero1)
+
+
+
+
+
+
+

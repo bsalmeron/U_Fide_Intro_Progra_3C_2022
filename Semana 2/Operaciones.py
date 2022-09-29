6bd94add8a22abf759d67da84ee06ad9334a6fe0
bsalmeron <30452732+bsalmeron@users.noreply.github.com>
2022-09-28 18:13:54 -0600

    Create Operaciones.py

---
+++ Semana 2/Operaciones.py
@@ -0,0 +1,26 @@
+
+suma = 1+1
+resta = 10-15
+
+division = 10//2
+potencia = 3**2
+
+print('La suma es: ', suma)
+print('La resta es: ', resta)
+print('La division es: ', division)
+print('La p0tencia es: ', potencia)
+
+operacion = ((5*4) - 5) / 3
+print(operacion)
+
+
+print(1==1)
+print(1==2)
+print('Amarillo'!='Verde')
+nombre='Samuel'
+print(nombre)
+
+
+operacion = 12 >((1+3)*(4-2)) or 1>3
+
+print(operacion)
